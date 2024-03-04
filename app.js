4bdb42fc865c67a3e9aecf766f8b67974d7d2d1c
AndreVanDerMee <144801062+AndreVanDerMee@users.noreply.github.com>
2024-03-04 21:53:25 +0100

    password

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 function alertButton(){
     alert("My name is Andre!")
 }
+
+function checkInput() {
+    var input = document.getElementById('secretInput').value;
+    if (input === "poops") {
+        // Change 'nextpage.html' to the actual next page URL
+        window.location.href = 'nextpage.html';
+    } else {
+        alert("Incorrect secret word. Please try again.");
+    }
+}
