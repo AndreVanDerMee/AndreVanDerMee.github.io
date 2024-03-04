b4e55ec28724656ce2459a9082fd75c74cc5e62e
AndreVanDerMee <144801062+AndreVanDerMee@users.noreply.github.com>
2024-03-04 22:04:30 +0100

    goro

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ function alertButton(){
 }
 
 function checkInput() {
-    var input = document.getElementById('secretInput').value;
+    var input = document.getElementById('secretInput').value.toLowerCase();
     if (input === "poops") {
-        // Change 'nextpage.html' to the actual next page URL
         window.location.href = 'nextpage.html';
     } else {
         alert("Incorrect secret word. Please try again.");
